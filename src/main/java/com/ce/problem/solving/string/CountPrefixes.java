8005908dcad1748bb0cdf7d946b1cbfa53cb53e7
Girma Moges Teklemariam <girma.moges.teklemariam21@gmail.com>
2022-05-05 16:25:45 -0500

    Add questions of Count Prefixes

--- src/main/java/com/ce/problem/solving/string/CountPrefixes.java
+++ src/main/java/com/ce/problem/solving/string/CountPrefixes.java
@@ -2,6 +2,14 @@ package com.ce.problem.solving.string;
 
 import java.util.Arrays;
 
+// You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+//Return the number of strings in words that are a prefix of s.
+//A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+// Constraints:
+//1 <= words.length <= 1000
+//1 <= words[i].length, s.length <= 10
+//words[i] and s consist of lowercase English letters only.
+
 public class CountPrefixes {
 
     public static void main(String[] args) {
